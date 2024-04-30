9e8021e13e6b28f0f503fb4701b5858e1958f3b6
Evgeny Starikov <genestar@yandex.ru>
2024-04-30 18:34:03 +0300

    BonusMilesService class created, main.java modified

---
+++ src/BonusMilesService.java
@@ -0,0 +1,15 @@
+public class BonusMilesService {
+
+    public int calculate(int price) {
+
+        int mil_eqwiwalent = 20;
+
+        int bonus = price / mil_eqwiwalent;
+
+        return  bonus;
+
+    }
+
+}
+
+
